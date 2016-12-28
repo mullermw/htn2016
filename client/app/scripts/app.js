9e1868b69b3e2ff97d7120681d303df1a4e9232b
Matthew Muller <matt.muller@qiagen.com>
2016-12-28 11:54:47 -0800

    switching from ngRoute to ui-router

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -13,24 +13,24 @@ angular
     'ngAnimate',
     'ngCookies',
     'ngResource',
-    'ngRoute',
     'ngSanitize',
     'ngTouch',
-    'ui.bootstrap'  
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
+    'ui.bootstrap',
+    'ui.router'
+  ]).config(function ($stateProvider) {
+	  $stateProvider.state({
+      name: 'home',
+      url: '/home',
+      templateUrl: 'views/home.html'
+    });
+    $stateProvider.state({
+      name: 'tests',
+      url: '/tests',
+      templateUrl: 'views/tests.html'
+    });
+    $stateProvider.state({
+      name: 'about',
+      url: '/about',
+      templateUrl: 'views/about.html'
+    });
   });
